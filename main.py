0f9f35d91a6a9170a22afb16c76d1cd4b8cb4a7c
Luis <eduardoguachilema24@gmail.com>
2022-07-27 01:54:44 -0500

    Creación del modulo main.py para hacer el llamda a la interfaz login(interfaz central) el cual tendrá enlazado las demás interfaces

---
+++ main.py
@@ -0,0 +1,11 @@
+from InterfazLogin import *
+if __name__=="__main__":
+    '''
+    if __name__=="__main__":
+        Main del programa, se encarga de crear la ventana principal, y ejecutar la primera interfaz "Login".
+        El cual es la que se encarga de iniciar sesión o registrarse, para los administradores del callcenter.
+    '''
+    app= QApplication([])
+    window=MainApp3()
+    window.show()
+    app.exec_()
